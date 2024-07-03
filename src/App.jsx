47084999beaf0b749df325e5007d0463f709cf9f
Your Name <you@example.com>
2024-07-04 03:47:37 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero"
+import Highlight from "./components/Highlight"
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+
+  return (
+    <>
+     <main className="bg-black" >
+     < Navbar/>
+      <Hero/>
+      <Highlight/>
+      </main> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
+import { hightlightsSlides } from "../constants";
+import gsap from "gsap";
+
+const VideoCarousel = () => {
+  const videoRaf = useRef([]);
+  const videoSpanRaf = useRef([]);
+  const videoDivRaf = useRef([]);
+
+  const [video, setvideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  const [loadedData, setloadedData] = useState([]);
+
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRaf.current[videoId].pause();
+      } else {
+        startPlay && videoRaf.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
+
+  useEffect(() => {
+    const currentProgress = 0;
+    let span = videoSpanRaf.current;
+
+    if (span[videoId]) {
+      // animate the pregress of the video'
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+        onComplete: () => {},
+      });
+    }
+  }, [videoId, startPlay]);
+
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list, i) => (
+          <div key={list.id} id="slider" className=" sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video id="video" playsInline preload="auto" muted ref={(el) => (videoRaf.current[i] = el)}
+                onPlay={() =>{
+                    setvideo((prevVideo) =>({
+                        ...prevVideo , isPlaying:true
+                    }))
+                }}
+                >
+              <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%]">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
