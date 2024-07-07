8f9f9bcf8de64d1596233301af3b94f54190fd73
Your Name <you@example.com>
2024-07-08 02:19:07 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlight from "./components/Highlight"
+import HowitWorks from "./components/HowitWorks"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
 
-const App = () => {
-
+const App = () =>{ 
   return (
     <>
      <main className="bg-black" >
@@ -13,6 +15,9 @@ const App = () => {
       <Hero/>
       <Highlight/>
       <Model/>
+      <Features/>
+      <HowitWorks/>
+      <Footer/>
       </main> 
     </>
   )
